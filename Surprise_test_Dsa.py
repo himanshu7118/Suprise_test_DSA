a94f62db2807f7232aed7e1ec8fc96da99e244a6
himanshu7118 <75970378+himanshu7118@users.noreply.github.com>
2023-06-05 21:04:26 +0530

    Update and rename Surprise_test_Dsa.ipynb to Surprise_test_Dsa.py

---
+++ Surprise_test_Dsa.py
@@ -0,0 +1,65 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+# Example 1:
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+# Constraints:
+# a. 1 <= nums.length <= 10^4
+# b. -2^31 <= nums[i] <= 2^31 - 1
+
+# Ans:
+
+def moveZeroes(nums):
+    left = 0
+    right = 0
+
+    while right < len(nums):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            
+        right += 1
+
+    return nums
+  
+ 
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+# Example 1:
+# Input: s = "leetcode"
+# Output: 0
+
+# Example 2:
+# Input: s = "loveleetcode"
+# Output: 2
+
+# Example 3:
+# Input: s = "aabb"
+# Output: -1
+
+# Constraints:
+# a. 1 <= s.length <= 10^5
+# b. s consists of only lowercase English letters.
+
+# Ans: 
+def firstUniqChar(s):
+    # Create a hashmap to store character frequencies
+    char_freq = {}
+
+    # Populate the hashmap
+    for char in s:
+        char_freq[char] = char_freq.get(char, 0) + 1
+
+    # Find the first non-repeating character and return its index
+    for i in range(len(s)):
+        if char_freq[s[i]] == 1:
+            return i
+
+    return -1
+
+  
+
+
